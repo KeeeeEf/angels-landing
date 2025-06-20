a428579100765c5af917ee1dc3681f64e687915f
KeeeeEf <kfrgallego@gmail.com>
2025-06-20 23:43:09 +0800

    feat: header profile menu

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,17 +1,25 @@
-import { Box, Button, IconButton, keyframes, Link, Modal, Paper, Typography, useTheme } from "@mui/material";
+import { Box, Button, IconButton, keyframes, Link, Menu, MenuItem, Modal, Paper, Typography, useTheme } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import AngelsLogo from "../../images/angels_logo.webp";
 import WalletIcon from "../../images/icons/wallet"
 import {getWallets} from "../../scripts/bifrost";
 import CloseIcon from '@mui/icons-material/Close';
 import { CardanoWallet } from "../../scripts/types";
+import ProfileIcon from "../../images/icons/profile";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DisconnectIcon from "../../images/icons/disconnect";
 
 const Header: React.FC = () => {
     const theme = useTheme()
 
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedWallet, setSelectedWallet] = useState<CardanoWallet | undefined>();
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [selectedWallet, setSelectedWallet] = useState<CardanoWallet | null>(null);
     const [scrolled, setScrolled] = useState(false);
+    const [copied, setCopied] = useState(false);
+
+    const profileMenuOpen = Boolean(anchorEl)
     
     const gradientShift = keyframes`
         0%   { background-position:   0% 50%; }
@@ -21,13 +29,31 @@ const Header: React.FC = () => {
 
     let WalletList = getWallets();
 
+    const handleProfileMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleProfileMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const switchWallet = () => {
+        setIsModalOpen(true);
+        handleProfileMenuClose();
+
+    }
+
+    const disconnectWallet = () => {
+        setSelectedWallet(null);
+        handleProfileMenuClose();
+    }
+
     async function connectWallet(wallet: CardanoWallet)
     {
         setSelectedWallet(wallet);
         setIsModalOpen(false);
         const api = await wallet.enable();
         const changeAddress = await api.getChangeAddress();
-        
     }
 
     useEffect(() => {
@@ -55,15 +81,160 @@ const Header: React.FC = () => {
                 </div>
                 <div>
                 {selectedWallet ? (
-                    <Typography
-                        sx={{
-                        color: theme.palette.text.primary,
-                        fontWeight: 500
-                        }}
-                        className="!text-sm capitalize"
-                    >
-                        Hello World
-                    </Typography>
+                    <div className="flex items-center gap-1">
+                        <div
+                            style={{
+                                backgroundColor: theme.palette.primary.main,
+                                borderRadius: "12px 4px 4px 12px"
+                                
+                            }}
+                            className="pl-4 py-[10px] pr-3 flex items-center gap-2"
+                        >
+                            <ProfileIcon 
+                                sx={{
+                                    color: theme.palette.secondary.dark
+                                }}
+                                className="!text-[20px]"
+                            />
+                            <Typography
+                                sx={{
+                                    color: theme.palette.secondary.dark,
+                                    fontWeight: 500
+                                }}
+                                className="!text-sm"
+                            >
+                                addr1q
+                            </Typography>
+                        </div>
+                        <div>
+                            <Button
+                                sx={{
+                                    backgroundColor: theme.palette.primary.main,
+                                    borderRadius: "4px 12px 12px 4px"
+                                }}
+                                className="!py-2 !min-w-12"
+                                aria-controls={profileMenuOpen ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={profileMenuOpen ? 'true' : undefined}
+                                onClick={handleProfileMenuClick}
+                            >
+                                <KeyboardArrowDownIcon sx={{color: theme.palette.secondary.dark}} className=""/>
+                            </Button>
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={profileMenuOpen}
+                                onClose={handleProfileMenuClose}
+                                slotProps={{
+                                list: {
+                                    'aria-labelledby': 'basic-button',
+                                },
+                                }}
+                                sx={{
+                                    borderRadius: "16px"
+                                }}
+                                PaperProps={{
+                                    sx: {
+                                        borderRadius: '16px',
+                                    },
+                                }}
+                            >
+                                <div className="w-80 !rounded-2xl">
+                                    <div 
+                                        style={{
+                                            borderBottom: `1px solid ${theme.palette.grey[300]}`
+                                        }}
+                                        className="flex items-center justify-between !p-4"
+                                    >
+                                        <div className="w-10 aspect-square">
+                                            <img
+                                                src={selectedWallet.icon}
+                                                alt="wallet icon"
+                                            />
+                                        </div>
+                                        <div>
+                                            <Typography
+
+                                            >
+                                                addr1q...nnfyr123vd13
+                                            </Typography>
+                                        </div>
+                                        <div>
+                                            <IconButton>
+                                                <ContentCopyIcon sx={{color: theme.palette.grey[100]}} className="!text-[20px]"/>
+                                            </IconButton>
+                                        </div>
+                                    </div>
+                                    <div
+                                        className="my-2 p-4"
+                                    >
+                                        <div 
+                                            style={{backgroundColor: theme.palette.grey[400]}} 
+                                            className="flex flex-col items-center justify-center rounded-xl h-24"
+                                        >
+                                            <Typography>
+                                                Wallet Balance
+                                            </Typography>
+                                            <Typography
+                                                sx={{
+                                                    fontWeight: 800,
+                                                    color: theme.palette.primary.light
+                                                }}
+                                                className="!text-[32px]"
+                                            >
+                                                ₳364.44
+                                            </Typography>
+                                        </div>
+                                    </div>
+                                    <div
+                                        style={{
+                                            borderTop: `1px solid ${theme.palette.grey[300]}`
+                                        }}
+                                        className="p-4"
+                                    >
+                                        <MenuItem 
+                                            sx={{
+                                                "&:hover":{
+                                                    backgroundColor: theme.palette.grey[500]
+                                                }
+                                            }}
+                                            className="!gap-2 !rounded-xl !py-4"
+                                            onClick={switchWallet}
+                                        >
+                                            <WalletIcon
+                                                sx={{
+                                                    color: theme.palette.text.primary
+                                                }}
+                                                className="!text-2xl"
+                                            />
+                                            <Typography>
+                                                Switch Wallet
+                                            </Typography>
+                                        </MenuItem>
+                                        <MenuItem 
+                                            sx={{
+                                                "&:hover":{
+                                                    backgroundColor: theme.palette.error.main
+                                                }
+                                            }}
+                                            className="!gap-2 !rounded-xl !py-4"
+                                            onClick={disconnectWallet}
+                                        >
+                                            <DisconnectIcon
+                                                sx={{
+                                                    color: theme.palette.text.primary
+                                                }}
+                                                className="!text-2xl"
+                                            />
+                                            <Typography>
+                                                Disconnect Wallet
+                                            </Typography>
+                                        </MenuItem>
+                                    </div>
+                                </div>
+                            </Menu>
+                        </div>
+                    </div>
                     ) : (
                     <Button 
                         onClick={() => setIsModalOpen(true)}
